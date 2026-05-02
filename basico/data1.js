064adba147a23a4e9c5794a707b37187f5496be1
Nicolas <nicolasbackprogrammer@gmail.com>
2026-05-02 17:48:43 -0300

    Finalização das Data

---
+++ basico/data1.js
@@ -0,0 +1,53 @@
+// Inteiros
+console.log(3);
+console.log(5);
+console.log(-65)
+
+// Flutuantes
+console.log(3.14);
+console.log(7,2);
+console.log(-14.5);
+
+// Strings
+console.log("Hello JacaScript World!");
+
+// Boolena
+const javaScript = true;
+
+// Symbol
+Symbol('meuSímbolo');
+
+// BigInt
+let numero = BigInt(1234567890123456789012345678901234567890n);
+
+// Objeto
+let pessoa = {
+    nome: "Alice",
+    numero: 30
+};
+
+
+let idade;
+console.log(idade); // Resultado undefined
+
+idade = 25;
+console.log(idade); // 25
+
+idade = 30 // Com let, o valor de idade muda 
+console.log(idade);
+
+// Nomes válidos para variaveis
+let pontos;
+let _numJogo;
+let $total;
+
+// Inválidos
+// let 1stPlace; Começa com número
+
+let Idade = 30;
+// Atenção as letras maisculas, serão consideradas uma nova variavel
+
+// Let e Const
+let pontoJogo = 25;
+const pontosMaximo = 100;
+
